e32fead1ee3436cb5fed578b730c5cbbb859a32d
daniel <exodaniel@gmail.com>
2020-11-17 16:50:43 +0100

    update setup for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 import numpy
-from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
-import os
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
 
 setup(
     name="pymap2d",
     description='Tools for 2D maps',
     author='Daniel Dugas',
-    version='1.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/danieldugas/pymap2d",
+    version='0.1',
     py_modules=['map2d', 'pose2d', 'circular_index', 'map2d_ros_tools'],
-    ext_modules = cythonize("CMap2D.pyx", annotate=True),
+    ext_modules=cythonize("CMap2D.pyx", annotate=True),
     include_dirs=[numpy.get_include()],
 )
